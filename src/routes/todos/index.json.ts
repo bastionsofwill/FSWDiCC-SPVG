70c90e8c6efc513944fce97d37d449c168c33a90
bastionsofwill <bastionsofwill@gmail.com>
2022-03-13 08:36:50 +0000

    Add GET and POST endpoints.

---
+++ src/routes/todos/index.json.ts
@@ -0,0 +1,27 @@
+import type { RequestHandler } from "@sveltejs/kit"
+
+// TODO: Persist in database
+let todos: Todo[] = [];
+
+export const get: RequestHandler = () => {
+    return {
+        status: 200,
+        body: todos
+    }
+}
+
+export const post: RequestHandler = async ({ request }) => {
+    const formData = await request.formData();
+    todos.push({
+        created_at: new Date(),
+        text: formData.get("text") as string,
+        done: false
+    });
+    return {
+        status: 303,
+        headers: {
+            location: "/"
+        },
+        body: formData.get("text") as string
+    }
+}
